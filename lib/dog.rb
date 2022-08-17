8285dd862b6f7381393dec1484a0d4a67a8e18eb
Kpine-MO <chrispine.oduor@student.moringaschool.com>
2022-08-17 23:48:32 +0300

    initial commit

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,28 @@
 class Dog
 
+  def name=(name)
+    @name = name
+  end
+
+  def breed=(breed)
+    @breed = breed
+  end
+
+  def name
+    @name
+  end
+
+  def breed
+    @breed
+  end
 end
+
+d1 = Dog.new
+
+d1.name = "snoopy"
+d1.name
+
+d1.breed = "Great Dane"
+d1.breed
+
+
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,25 @@
 class Person
-
+    def name=(name)
+        @name = name
+      end
+    
+      def job=(job)
+        @job = job
+      end
+    
+      def name
+        @name
+      end
+    
+      def job
+        @job
+      end
 end
+
+p1 = Person.new
+
+p1.name = "Chrispine"
+p1.name
+
+p1.job = "Programmer"
+p1.job
